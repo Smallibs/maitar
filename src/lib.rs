21a43976d9f661b048a82ca73022809f032e4410
d-plaindoux <d.plaindoux@free.fr>
2023-08-13 21:35:45 +0200

    Add Result implementations in standard module

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,12 @@ mod standard;
 #[cfg(test)]
 mod tests {
     use crate::specs::monad::Monad;
-    use crate::standard::option::OptionK;
+    use crate::standard::result::ResultK;
 
     #[test]
     fn it_works() {
-        let ma = Some(1);
-        let mb = OptionK::bind(|a| { Some(a + 1) }, ma);
-        assert_eq!(mb, Some(2))
+        let ma = Ok(1);
+        let mb = <ResultK<String> as Monad>::bind(|a| { Ok(a + 1) }, ma);
+        assert_eq!(mb, Ok(2))
     }
 }
---
+++ src/standard/result.rs
@@ -0,0 +1,44 @@
+use std::marker::PhantomData;
+use crate::specs::applicative::Applicative;
+use crate::specs::functor::Functor;
+use crate::specs::hkp::HKP;
+use crate::specs::monad::Monad;
+
+pub struct ResultK<E> {
+    _ignore: PhantomData<E>,
+}
+
+impl<E> HKP for ResultK<E> {
+    type T<A> = Result<A, E>;
+}
+
+impl<E> Functor for ResultK<E> {
+    fn map<A, B>(f: fn(A) -> B, ma: Self::T<A>) -> Self::T<B> {
+        match ma {
+            Ok(a) => Ok(f(a)),
+            Err(e) => Err(e),
+        }
+    }
+}
+
+impl<E> Applicative for ResultK<E> {
+    fn pure<A>(a: A) -> Self::T<A> {
+        Ok(a)
+    }
+
+    fn apply<A, B>(mf: Self::T<fn(A) -> B>, ma: Self::T<A>) -> Self::T<B> {
+        match mf {
+            Ok(f) => ResultK::map(f, ma),
+            Err(e) => Err(e),
+        }
+    }
+}
+
+impl<E> Monad for ResultK<E> {
+    fn join<A>(mma: Self::T<Self::T<A>>) -> Self::T<A> {
+        match mma {
+            Ok(ma) => ma,
+            Err(e) => Err(e),
+        }
+    }
+}
