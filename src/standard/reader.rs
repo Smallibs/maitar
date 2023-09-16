7262560f8ae456cc16f588bf5fd1c95db4495dc7
d-plaindoux <d.plaindoux@free.fr>
2023-09-16 09:43:50 +0200

    Reader - Applicative - pure

--- src/standard/reader.rs
+++ src/standard/reader.rs
@@ -1,10 +1,12 @@
 use crate::core::hkp::HKP;
+use crate::specs::applicative::Applicative;
+use crate::specs::bind::Bind;
 use crate::specs::functor::Functor;
 use std::marker::PhantomData;
 
-pub struct Reader<'a, E, F: HKP<'a>, A: 'a>(F, Box<dyn FnOnce(E) -> F::T<A> + 'a>);
+pub struct Reader<'a, E, F: HKP<'a>, A: 'a>(Box<dyn FnOnce(E) -> F::T<A> + 'a>, PhantomData<F>);
 
-pub struct ReaderK<'a, E, F: HKP<'a>>(PhantomData<&'a E>, PhantomData<F>);
+pub struct ReaderK<'a, E, F: HKP<'a>>(F, PhantomData<&'a E>);
 
 impl<'a, E, F: HKP<'a>> HKP<'a> for ReaderK<'a, E, F> {
     type T<B: 'a> = Reader<'a, E, F, B>;
@@ -15,8 +17,22 @@ impl<'e, E, F: Functor<'e> + 'e> Functor<'e> for ReaderK<'e, E, F> {
     where
         MAP: Fn(A) -> B + 'e,
     {
-        let Reader(functor, va) = ma;
+        let Reader(va, _) = ma;
         let run = |e| F::map::<A, B, MAP>(f, va(e));
-        Reader(functor, Box::new(run))
+        Reader(Box::new(run), PhantomData)
+    }
+}
+
+impl<'e, E, F: Applicative<'e> + 'e> Applicative<'e> for ReaderK<'e, E, F> {
+    fn pure<A: 'e>(a: A) -> Self::T<A> {
+        Reader(Box::new(|_| F::pure(a)), PhantomData)
+    }
+
+    fn apply<A, B, MAP>(mf: Self::T<MAP>, ma: Self::T<A>) -> Self::T<B>
+    where
+        A: Clone,
+        MAP: Fn(A) -> B,
+    {
+        todo!()
     }
 }
