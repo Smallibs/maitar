65b0a3510900aa2ee0fc04e96fbf97401ce39a83
d-plaindoux <d.plaindoux@free.fr>
2023-09-17 17:57:48 +0200

    Writer functor

--- src/standard/writer.rs
+++ src/standard/writer.rs
@@ -5,6 +5,7 @@
  */
 
 use crate::core::hkp::HKP;
+use crate::specs::functor::Functor;
 use std::marker::PhantomData;
 
 pub struct Writer<'a, E: 'a, F: HKP<'a>, A: 'a>(F::T<(A, E)>);
@@ -13,4 +14,14 @@ pub struct WriterK<'a, E, F: HKP<'a>>(PhantomData<&'a E>, PhantomData<F>);
 
 impl<'a, E, F: HKP<'a>> HKP<'a> for WriterK<'a, E, F> {
     type T<B: 'a> = Writer<'a, E, F, B>;
+}
+
+impl<'e, E, F: Functor<'e>> Functor<'e> for WriterK<'e, E, F> {
+    fn map<A, B, MAP>(f: MAP, ma: Self::T<A>) -> Self::T<B>
+    where
+        MAP: Fn(A) -> B + 'e,
+    {
+        let Writer(va) = ma;
+        Writer(F::map(move |(a, e)| (f(a), e), va))
+    }
 }
